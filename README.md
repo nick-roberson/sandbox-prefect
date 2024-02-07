2325a27c3849c0fef0ddc5973e3ec460acb6589a
nick-roberson <nicholas.roberson.95@gmail.com>
2024-02-07 11:35:21 -0500

    update readme

--- README.md
+++ README.md
@@ -1,12 +1,45 @@
 Testing out a locally running prefect server. 
 
-How to start server (in one terminal window)
+### Start Server (in one terminal window)
 ```
 $ prefect server start
-```
 
-How to run test pipeline (in another terminal window)
+...
+
+
+ ___ ___ ___ ___ ___ ___ _____
+| _ \ _ \ __| __| __/ __|_   _|
+|  _/   / _|| _|| _| (__  | |
+|_| |_|_\___|_| |___\___| |_|
+
+Configure Prefect to communicate with the server with:
+
+    prefect config set PREFECT_API_URL=http://127.0.0.1:4200/api
+
+View the API reference documentation at http://127.0.0.1:4200/docs
+
+Check out the dashboard at http://127.0.0.1:4200
+```
+And then go to that URL: http://127.0.0.1:4200
+
+### Run Test Pipeline
 ```
 $ poetry install
 $ poetry run python nick-prefect/test_prefect.py
+```
+
+Run with an argument
+```
+$ poetry run python nick-prefect/test_prefect.py --name "nick-roberson/places"
+
+13:23:57.991 | INFO    | prefect.engine - Created flow run 'thundering-flounder' for flow 'get-repo-info'
+13:23:58.023 | INFO    | Flow run 'thundering-flounder' - Created task run 'get_url-0' for task 'get_url'
+13:23:58.024 | INFO    | Flow run 'thundering-flounder' - Executing 'get_url-0' immediately...
+13:23:58.447 | INFO    | Task run 'get_url-0' - Finished in state Completed()
+13:23:58.447 | WARNING | Task run 'get_url-0' - Task run 'get_url-0' finished in state Completed()
+13:23:58.448 | INFO    | Flow run 'thundering-flounder' - PrefectHQ/prefect repository statistics 🤓:
+13:23:58.449 | INFO    | Flow run 'thundering-flounder' - Stars 🌠 : 1
+13:23:58.449 | INFO    | Flow run 'thundering-flounder' - Forks 🍴 : 0
+13:23:58.464 | INFO    | Flow run 'thundering-flounder' - Finished in state Completed()
+Result: True
 ```
--- nick-prefect/test_prefect.py
+++ nick-prefect/test_prefect.py
@@ -1,9 +1,27 @@
 import httpx
+import pydantic
+from typing import Dict, Union
 from prefect import flow, task, get_run_logger
 
+
+class Repo(pydantic.BaseModel):
+    name: str
+    stargazers_count: int
+    forks_count: int
+
+    # Ignore unrecognized fields
+    class Config:
+        extra = "ignore"
+
+
+class Response(pydantic.BaseModel):
+    repo: Union[Repo, None]
+    error: Union[str, None]
+
+
 @task
-def get_url(url: str, params: dict = None):
-    """ Get the JSON response from a URL.
+def get_url(url: str, params: dict = None) -> Dict:
+    """Get the JSON response from a URL.
 
     Args:
         url (str): _description_
@@ -12,28 +30,66 @@ def get_url(url: str, params: dict = None):
     Returns:
         Dict: Response JSON
     """
-    response = httpx.get(url, params=params)
-    response.raise_for_status()
-    return response.json()
+    try:
+        # Get the response
+        response = httpx.get(url, params=params)
+        response.raise_for_status()
+
+        # Cast to models and return
+        repo: Repo = Repo(**response.json())
+        return Response(repo=repo, error=None)
+    except httpx.HTTPError as e:
+        return Response(repo=None, error=str(e))
+
 
 @flow(retries=3, retry_delay_seconds=5, log_prints=True)
-def get_repo_info(
-    repo_name: str = "PrefectHQ/prefect"
-):
-    """ Get the statistics of a GitHub repository.
+def get_repo_info(repo_name: str = "PrefectHQ/prefect"):
+    """Get the statistics of a GitHub repository.
 
     Args:
         repo_name (str, optional): _description_. Defaults to "PrefectHQ/prefect".
     """
+    # Get logger
+    logger = get_run_logger()
+
     # Get the repository information
     url = f"https://api.github.com/repos/{repo_name}"
-    repo = get_url(url)
-    
+    response: Response = get_url(url)
+
     # Log the statistics
-    logger = get_run_logger()
-    logger.info("PrefectHQ/prefect repository statistics 🤓:")
-    logger.info(f"Stars 🌠 : {repo['stargazers_count']}")
-    logger.info(f"Forks 🍴 : {repo['forks_count']}")
+    if response.error:
+        logger.error(f"Failed to get repository information: {response.error}")
+        return False
+    else:
+        logger.info("PrefectHQ/prefect repository statistics 🤓:")
+        logger.info(f"Stars 🌠 : {response.repo.stargazers_count}")
+        logger.info(f"Forks 🍴 : {response.repo.forks_count}")
+        return True
+
+
+def parse_args():
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description="Get the statistics of a GitHub repository."
+    )
+    parser.add_argument(
+        "--name",
+        type=str,
+        default="PrefectHQ/prefect",
+        help="The name of the repository to get the statistics.",
+    )
+    return parser.parse_args()
+
+
+def main():
+    """Main function to run the flow."""
+    # Get args and run the flow
+    args = parse_args()
+    result = get_repo_info(repo_name=args.name)
+    # Log the result
+    print(f"Result: {result}")
+
 
 if __name__ == "__main__":
-    get_repo_info()
+    main()
